7be9bf757bdda6524814ba32e318c23730593e7d
MikeSobrado <mike@trading-dome.dev>
2025-11-26 04:58:45 +0100

    Agregar header 'paptrading' a CORS allowedHeaders

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ const corsOptions = {
   },
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
+  allowedHeaders: ['Content-Type', 'Authorization', 'paptrading']
 };
 
 app.use(cors(corsOptions));
